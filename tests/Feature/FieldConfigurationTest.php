19a3544d0723a85e5053633b9ee9fc4557df470a
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-05-11 16:33:16 +0200

    chore: fix tests

--- tests/Feature/FieldConfigurationTest.php
+++ tests/Feature/FieldConfigurationTest.php
@@ -5,7 +5,29 @@ declare(strict_types=1);
 use Innobrain\Structure\Facades\FieldConfiguration;
 
 it('should be able to retrieve the field configuration', function () {
-    $fields = FieldConfiguration::retrieveForClient(env('ONOFFICE_TOKEN'), env('ONOFFICE_SECRET'));
+    $file = file_get_contents(Pest\testDirectory('Stubs/FieldsResponse.json'));
+    $json = json_decode($file, true);
+
+    Http::fake([
+        'https://api.onoffice.de/api/stable/api.php/' => Http::response($json),
+    ]);
+
+    $fields = FieldConfiguration::retrieveForClient('test', 'test');
+
+    expect($fields)->toBeInstanceOf(Illuminate\Support\Collection::class)
+        ->and($fields->first())->toBeInstanceOf(Innobrain\Structure\DTOs\Module::class);
+
+});
+
+it('should be able to retrieve the field configuration for client 2', function () {
+    $file = file_get_contents(Pest\testDirectory('Stubs/FieldsResponse2.json'));
+    $json = json_decode($file, true);
+
+    Http::fake([
+        'https://api.onoffice.de/api/stable/api.php/' => Http::response($json),
+    ]);
+
+    $fields = FieldConfiguration::retrieveForClient('test', 'test');
 
     expect($fields)->toBeInstanceOf(Illuminate\Support\Collection::class)
         ->and($fields->first())->toBeInstanceOf(Innobrain\Structure\DTOs\Module::class);
