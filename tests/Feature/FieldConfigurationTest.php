ed5a04db8023cfcb8632c65038274a8705d809c6
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-05-18 15:56:08 +0200

    chore: test

--- tests/Feature/FieldConfigurationTest.php
+++ tests/Feature/FieldConfigurationTest.php
@@ -2,20 +2,28 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Collection;
+use Innobrain\Structure\DTOs\Field;
+use Innobrain\Structure\DTOs\FieldDependency;
+use Innobrain\Structure\DTOs\FieldFilter;
+use Innobrain\Structure\DTOs\Module;
+use Innobrain\Structure\DTOs\PermittedValue;
 use Innobrain\Structure\Facades\FieldConfiguration;
 
 it('should be able to retrieve the field configuration', function () {
     $file = file_get_contents(Pest\testDirectory('Stubs/FieldsResponse.json'));
     $json = json_decode($file, true);
 
-    Http::fake([
-        'https://api.onoffice.de/api/stable/api.php/' => Http::response($json),
+    Illuminate\Support\Facades\Http::fake([
+        'https://api.onoffice.de/api/stable/api.php/' => Illuminate\Support\Facades\Http::response($json),
     ]);
 
-    $fields = FieldConfiguration::retrieveForClient('test', 'test');
+    $modules = FieldConfiguration::retrieveForClient('test', 'test');
 
-    expect($fields)->toBeInstanceOf(Illuminate\Support\Collection::class)
-        ->and($fields->first())->toBeInstanceOf(Innobrain\Structure\DTOs\Module::class);
+    expect($modules)->toBeInstanceOf(Collection::class)
+        ->and($modules->first())->toBeInstanceOf(Module::class)
+        ->and($modules->first()->fields)->toBeInstanceOf(Collection::class)
+        ->and($modules->first()->fields->first())->toBeInstanceOf(Field::class);
 
 });
 
@@ -23,13 +31,15 @@ it('should be able to retrieve the field configuration for client 2', function (
     $file = file_get_contents(Pest\testDirectory('Stubs/FieldsResponse2.json'));
     $json = json_decode($file, true);
 
-    Http::fake([
-        'https://api.onoffice.de/api/stable/api.php/' => Http::response($json),
+    Illuminate\Support\Facades\Http::fake([
+        'https://api.onoffice.de/api/stable/api.php/' => Illuminate\Support\Facades\Http::response($json),
     ]);
 
-    $fields = FieldConfiguration::retrieveForClient('test', 'test');
+    $modules = FieldConfiguration::retrieveForClient('test', 'test');
 
-    expect($fields)->toBeInstanceOf(Illuminate\Support\Collection::class)
-        ->and($fields->first())->toBeInstanceOf(Innobrain\Structure\DTOs\Module::class);
-
+    expect($modules)->toBeInstanceOf(Collection::class)
+        ->and($modules->first())->toBeInstanceOf(Module::class)
+        ->and($modules->first()->fields)->toBeInstanceOf(Collection::class)
+        ->and($modules->first()->fields->first())->toBeInstanceOf(Field::class)
+        ->and($modules->first()->fields->get('Anrede-Titel')->filters->first())->toBeInstanceOf(FieldFilter::class);
 });
