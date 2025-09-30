c6f5c45216574e227e369388dcdf9b3284721c33
Bruno Görß <bruno.goerss@inno-brain.de>
2025-09-30 21:57:21 +0200

    build: add pest type coverage command

--- src/Converters/Array/ArrayField.php
+++ src/Converters/Array/ArrayField.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Innobrain\Structure\Converters\Array;
 
 use Innobrain\Structure\Dtos\Field;
+use Innobrain\Structure\Dtos\FieldDependency;
+use Innobrain\Structure\Dtos\FieldFilter;
+use Innobrain\Structure\Dtos\PermittedValue;
 
 trait ArrayField
 {
@@ -19,14 +22,14 @@ trait ArrayField
             'type' => $field->type->value,
             'length' => $field->length,
             'permittedValues' => $field->permittedValues
-                ->map(fn ($pv) => $pv->convert($this))
+                ->map(fn (PermittedValue $pv) => $pv->convert($this))
                 ->toArray(),
             'default' => $field->default,
             'filters' => $field->filters
-                ->map(fn ($f) => $f->convert($this))
+                ->map(fn (FieldFilter $f) => $f->convert($this))
                 ->toArray(),
             'dependencies' => $field->dependencies
-                ->map(fn ($d) => $d->convert($this))
+                ->map(fn (FieldDependency $d) => $d->convert($this))
                 ->toArray(),
             'compoundFields' => $field->compoundFields->toArray(),
             'fieldMeasureFormat' => $field->fieldMeasureFormat,
