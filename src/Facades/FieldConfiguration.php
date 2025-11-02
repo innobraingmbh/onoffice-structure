95e71ec61480da13dab848873457884beb9090d3
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-11-02 09:44:40 +0100

    chore: docblocks

--- src/Facades/FieldConfiguration.php
+++ src/Facades/FieldConfiguration.php
@@ -7,12 +7,13 @@ namespace Innobrain\Structure\Facades;
 use Illuminate\Support\Facades\Facade;
 use Innobrain\OnOfficeAdapter\Dtos\OnOfficeApiCredentials;
 use Innobrain\Structure\Collections\ModulesCollection;
+use Innobrain\Structure\Enums\Language;
 use Innobrain\Structure\Services\FieldConfiguration as ServiceFieldConfiguration;
 
 /**
  * @see ServiceFieldConfiguration
  *
- * @method static ModulesCollection retrieveForClient(OnOfficeApiCredentials $credentials, array<int, string> $only = [])
+ * @method static ModulesCollection retrieveForClient(OnOfficeApiCredentials $credentials, array<int, string> $only = [], Language $language = Language::German)
  */
 class FieldConfiguration extends Facade
 {
--- src/Facades/Structure.php
+++ src/Facades/Structure.php
@@ -7,13 +7,14 @@ namespace Innobrain\Structure\Facades;
 use Illuminate\Support\Facades\Facade;
 use Innobrain\OnOfficeAdapter\Dtos\OnOfficeApiCredentials;
 use Innobrain\Structure\Collections\ModulesCollection;
+use Innobrain\Structure\Enums\Language;
 use Innobrain\Structure\Services\Structure as ServiceStructure;
 
 /**
  * @see ServiceStructure
  *
  * @method static ServiceStructure forClient(OnOfficeApiCredentials $onOfficeApiCredentials)
- * @method static ModulesCollection getModules(string|array<int, string> $only = [])
+ * @method static ModulesCollection getModules(string|array<int, string> $only = [], Language $language = Language::German)
  */
 class Structure extends Facade
 {
