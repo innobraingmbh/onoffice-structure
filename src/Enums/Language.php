8640702be706e1f65ae8b19dec3937166c0906dd
andre-onoffice <a.wussow@onoffice.de>
2026-06-26 14:43:28 +0200

    added support for more languages (#24)

--- src/Enums/Language.php
+++ src/Enums/Language.php
@@ -6,10 +6,38 @@ namespace Innobrain\Structure\Enums;
 
 enum Language: string
 {
-    case German = 'DEU';
-    case English = 'ENG';
-    case French = 'FRA';
-    case Spanish = 'ESP';
-    case Italian = 'ITA';
+    case Albanian = 'ALB';
+    case American = 'USA';
+    case Arabic = 'SAU';
+    case Austrian = 'AUT';
+    case Bulgarian = 'BGR';
+    case Catalan = 'CAT';
+    case Chinese = 'CHN';
     case Croatian = 'HRV';
+    case Czech = 'CZE';
+    case Danish = 'DNK';
+    case Dutch = 'NLD';
+    case English = 'ENG';
+    case Finnish = 'FIN';
+    case Flemish = 'BEL';
+    case French = 'FRA';
+    case German = 'DEU';
+    case Greek = 'GRC';
+    case Hungarian = 'HUN';
+    case Italian = 'ITA';
+    case Japanese = 'JPN';
+    case Korean = 'KOR';
+    case Lithuanian = 'LTU';
+    case Luxembourgish = 'LUX';
+    case Norwegian = 'NOR';
+    case Polish = 'POL';
+    case Portuguese = 'PRT';
+    case Romanian = 'ROU';
+    case Russian = 'RUS';
+    case Serbian = 'SRB';
+    case Slovenian = 'SVN';
+    case Spanish = 'ESP';
+    case Swedish = 'SWE';
+    case SwissGerman = 'CHE';
+    case Turkish = 'TUR';
 }
